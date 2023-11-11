5cfb3fe71cbb786d2d5a00d1d72d06089eedd191
Natalie Gillam <ngillam@hotmail.co.uk>
2023-11-11 23:56:14 +0000

    made quiz guide visible on clicking 'start quiz' button

---
+++ script.js
@@ -0,0 +1,7 @@
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
+
+startBtn.onclick = () => {
+ popupInfo.classList.add('active');
+
+}
