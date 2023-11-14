dd506b940f1725cc42c7080427ea10aeef671621
Natalie Gillam <ngillam@hotmail.co.uk>
2023-11-14 23:46:49 +0000

    Started adding questions

---
+++ questions.js
@@ -0,0 +1,7 @@
+let questions = [
+  { id: 1, question: "What does the accumulator hold?", answer: "B. The data that is being processed and the results of the processing.", options: [] },
+  { id: 2, question: "What does the current instruction register hold?", answer: "", options: ["B. The data that is being processed and the results of the processing."] },
+  { id: 3, question: "What does the memory address register hold?", answer: "", options: [] },
+  { id: 4, question: "What does the memory data register hold?", answer: "", options: ["C. The data that is being processed and the results of the processing."]},
+  { id: 5, question: "What does the program counter hold?", answer: "", options: [] },
+];
